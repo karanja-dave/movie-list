793f46858b3fcb3f40fbf77a650fe7d43018efde
Dave Karanja <ganjenz04@gmail.com>
2025-09-26 02:08:20 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,81 @@
+/**
+ * 
+ * 
+ * DOM: Document Object Model
+ * use js to interact with html elememnts
+ * 
+ *  task: we want to add a new movie to our list
+ *  
+ *  Steps:
+ *      -locate the <ul> element
+ *      - in each list add 2 spans for movie name and delete
+ */
+
+
+// code tells JS to wait for HTML to run b4 it runs 
+//otherwise the JS code should be written below the HTML code as we did previously
+document.addEventListener('DOMContentLoaded',function(){
+
+    // tells JS to look for an element labelled id="movie-list" then selecet the unordered list and store it object called list
+    const list=document.querySelector("#movie-list ul")
+
+    const forms=document.forms //gets all forms and store them in the form variable
+
+    // deleting movies
+    // let JS click on the delete button
+    list.addEventListener("click", function(e){
+        if(e.target.className='delete'){
+            const li=e.target.parentElement;
+            li.parentNode.removeChild(li)
+        }
+    })
+
+
+    // adding movies
+    const addMovieForm = forms['add-movie']
+    addMovieForm.addEventListener('submit',function(event){
+        event.preventDefault() //ensures pages does not refresh when we click the add button in our form
+
+        // code block 3 
+        const value = addMovieForm.querySelector('input[type="text"]').value
+        console.log(value); //for testing purposes-outputed in the console log
+
+        //alert user to enter movie name when they click add withou inputing movie
+        if(!value){
+            alert("Please enter movie name") //an alert message pops up to ask user to enter movie name 
+            console.log("Please add a movie name!");
+            return
+        }
+
+        // creating elements that store added movies, the unordered list 
+        const li=document.createElement('li') //using JS to create an element
+        const movieName=document.createElement('span') //tell JS to create a span for movie name
+        const deleteBtn=document.createElement('span') //tells JS to create a span for the delete button
+
+
+        // adding content in code block 3
+        movieName.textContent=value
+        deleteBtn.textContent='Delete'
+
+        // adding classes to the spans created
+        movieName.classList.add('name')
+        deleteBtn.classList.add('delete')
+
+        // appending spans to a list
+        li.appendChild(movieName)
+        li.appendChild(deleteBtn)
+
+        // appending new list to the unordered list (append to DOM)
+        list.appendChild(li)
+
+
+        // reset the form-clear form once you input movie and press Add button 
+        addMovieForm.reset()
+
+
+
+    })
+
+
+
+})
